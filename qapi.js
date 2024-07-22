28d75a26d39a6d7f45faa74348e6000c66793039
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-07-22 11:25:55 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"669e1776995d120018f22549",
+				"669e1776995d120018f2254c"
+			]
+		},
+		"669e1776995d120018f22549": {
+			"id": "669e1776995d120018f22549",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"669e1776995d120018f2254c": {
+			"id": "669e1776995d120018f2254c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
